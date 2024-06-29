67fff2e52915f44277519b40150adc5959963206
Soumalya Paul <samratghosh240@gmail.com>
2024-06-29 19:38:44 +0530

    Updated

---
+++ contracts/NFT/nft.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFT is ERC721 {
+    uint256 private _nextTokenId;
+
+    constructor()
+        ERC721("MyToken", "MTK")
+    {}
+
+    function safeMint() public {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(msg.sender, tokenId);
+    }
+}
